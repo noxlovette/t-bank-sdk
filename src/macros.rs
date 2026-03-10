e28fa2b55ec15beaa61f43489ee5396af8c66942
Danila Volkov <noxlovette@proton.me>
2026-03-10 10:54:21 +0300

    macros again

---
+++ src/macros.rs
@@ -0,0 +1,16 @@
+#[macro_export]
+macro_rules! impl_string_conversions_default {
+    ($type:ty) => {
+        impl From<String> for $type {
+            fn from(value: String) -> Self {
+                value.parse().unwrap_or_default()
+            }
+        }
+
+        impl From<Option<String>> for $type {
+            fn from(value: Option<String>) -> Self {
+                value.map(Into::into).unwrap_or_default()
+            }
+        }
+    };
+}
